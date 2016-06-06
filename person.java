d04185bcabff32c5de13d5e0a2bd0cc35a4d2b5c
Rickx777 <Cwong7@stuy.edu>
2016-06-05 22:28:08 -0400

    person class and test processing file added

---
+++ person.java
@@ -0,0 +1,9 @@
+public class person{
+	String name;
+	double disCh; //chance of disease
+	double bneCh; //chance of bone breaking
+	int imp; // impact on health (+-) 1 - 3
+	int health; // 1 -> 4 | very poor -> good
+	
+	
+}
